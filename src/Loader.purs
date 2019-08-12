da601c842806369f62dab4a7954b69e5b8f954b5
keiSunagawa <kei.sunagawa@mvrck.co.jp>
2019-08-12 16:15:10 +0900

    decode alias

--- src/Loader.purs
+++ src/Loader.purs
@@ -1,6 +1,7 @@
 module Loader
-       ( run2,
-         Hoge
+       ( run,
+         ValueType,
+         Template
        ) where
 
 import Prelude
@@ -8,13 +9,17 @@ import Control.Monad.Except (runExcept)
 import Data.Either (Either(..))
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
-import Foreign (F, Foreign, ForeignError(..), fail)
+import Foreign (F, Foreign, ForeignError(..), fail, readArray, readString)
 import Foreign.Index(readProp)
 import Foreign.Keys(keys)
 import Data.Function.Uncurried (Fn3, runFn3)
 import Foreign.Generic(genericDecode, Options)
 import Foreign.Generic.Class(defaultOptions)
 import Data.Bifunctor(lmap)
+import Data.Map(empty, lookup, insert, Map)
+import Data.Foldable(foldl)
+import Data.String(toLower)
+import Data.Traversable(sequence)
 
 foreign import parseYAMLImpl :: forall r. Fn3 (String -> r) (Foreign -> r) String r
 
@@ -48,9 +53,6 @@ newtype Hoge = Hoge
                }
 derive instance genericHoge :: Generic Hoge _
 instance showMyRecord :: Show Hoge where show = genericShow
-
-run :: Either String Hoge
-run = yamlToData yamlInput
 
 yamlInputProd :: String
 yamlInputProd = """
@@ -87,7 +89,60 @@ decodeF json =
       alias <- readProp "alias" json
       keys alias
 
-run2 :: Either String (Array String)
+
+data ValueType = SingeValue | ArrayValue
+instance showVT :: Show ValueType where
+  show SingeValue = "SingleValue"
+  show ArrayValue = "ArrayValue"
+
+type Template = Map String ValueType
+type AliasMap = Map String String
+
+foreignErrorToString :: forall a . F a -> Either String a
+foreignErrorToString fa = lmap show (runExcept $ fa)
+
+decodeTemplate :: Foreign -> Either String Template
+decodeTemplate json =
+  foreignErrorToString decoded
+  where
+    decoded = do
+      tmp <- readProp "template" json
+      xs <- readArray tmp
+      xs' <- sequence (decodeItem <$> xs)
+      pure $ foldl (\m -> \di -> insert di.label di.tpe m) empty xs'
+    decodeItem itemJson =
+      do
+        label <- readProp "label" itemJson >>= readString
+        tpeStr <- readProp "type" itemJson >>= readString
+        tpe <- case toLower tpeStr of
+          "single" -> pure SingeValue
+          "array" -> pure ArrayValue
+          utpe -> fail $ ForeignError $ "unknown type" <> utpe
+        pure {label: label, tpe: tpe}
+
+readMap :: forall a. Foreign -> (Foreign -> F a) -> F (Map String a)
+readMap json f = do
+  ks <- keys json
+  xs <- sequence $ toKV <$> ks
+  pure $ foldl (\m -> \kv -> insert kv.key kv.value m) empty xs
+  where
+    toKV key = readProp key json >>= f <#> \v -> {key: key, value: v}
+
+decodeAlias :: Foreign -> Either String AliasMap
+decodeAlias json =
+  foreignErrorToString decoded
+  where
+    decoded = do
+      als <- readProp "alias" json
+      readMap als readString
+
+run2 :: Either String Template
 run2 = do
   json <- yamlToJson yamlInputProd
-  decodeF json
+  decodeTemplate json
+
+run = do
+  json <- yamlToJson yamlInputProd
+  tmp <- decodeTemplate json
+  als <- decodeAlias json
+  pure {als: als, tmp: tmp}
--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,9 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (logShow)
-import Loader(run2)
+import Loader(run)
+import Data.Map(empty, lookup, insert, Map)
+
 main :: Effect Unit
 main = do
-  logShow run2
+  logShow run
